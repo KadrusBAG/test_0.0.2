33159286d1a0c992a92c598b0c3c4297607d4f4f
KadrusBAG <32191410+KadrusBAG@users.noreply.github.com>
2017-11-10 12:22:35 +0300

    Update 0.0.3.cpp

--- 0.0.3.cpp
+++ 0.0.3.cpp
@@ -29,6 +29,36 @@ bool input(float ** & matrix, unsigned int & rows, unsigned int & columns){
     return true;
 }
 
+void sdvig(float *array, int size, int sdv){
+    float *array_left=new float[size-sdv];
+    float *array_right=new float[sdv];
+    for(unsigned int i=0; i<size; ++i){
+        if(i<size-sdv){
+            array_left[i]=array[i];
+        }
+        else{
+            array_right[i+sdv-size]=array[i];
+        }
+    }
+    for(unsigned int i=0; i<(size-sdv)/2; ++i){
+        swap(array_left[i], array_left[size-sdv-1-i]);
+    }
+    for(unsigned int i=0; i<sdv/2; ++i){
+        swap(array_right[i], array_right[sdv-1-i]);
+    }
+    for(unsigned int i=0; i<size; ++i){
+        if(i<size-sdv){
+            array[i]=array_left[i];
+        }
+        else{
+            array[i]=array_right[i+sdv-size];
+        }
+    }
+    for(unsigned int i=0; i<size/2; ++i){
+        swap(array[i], array[size-1-i]);
+    }
+}
+
 int main(){
     float **matrix;
     unsigned int rows, columns;
@@ -51,7 +81,7 @@ int main(){
         line[el]=matrix[0][j];
         el++;
     }
-    for(unsigned int i=0; i<rows-1; ++j){
+    for(unsigned int i=0; i<rows-1; ++i){
         line[el]=matrix[i][columns-1];
         el++;
     }
@@ -59,7 +89,33 @@ int main(){
         line[el]=matrix[rows-1][j];
         el++;
     }
-    for(unsigned int i=rows-1; i>0; --j){
+    for(unsigned int i=rows-1; i>0; --i){
         line[el]=matrix[i][0];
         el++;
     }
+    sdvig(line, size, sdv);
+    el=0;
+    for(unsigned int j=0; j<columns-1; ++j){
+        matrix[0][j]=line[el];
+        el++;
+    }
+    for(unsigned int i=0; i<rows-1; ++i){
+        matrix[i][columns-1]=line[el];
+        el++;
+    }
+    for(unsigned int j=columns-1; j>0; --j){
+        matrix[rows-1][j]=line[el];
+        el++;
+    }
+    for(unsigned int i=rows-1; i>0; --i){
+        matrix[i][0]=line[el];
+        el++;
+    }
+    delete[]line;
+    for(unsigned int i=0; i<rows; ++i){
+        for(unsigned int j=0; j<columns; ++j){
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
