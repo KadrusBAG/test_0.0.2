09b17f0c157a07c3b3febe5f6e3e04ef235eb017
KadrusBAG <32191410+KadrusBAG@users.noreply.github.com>
2017-11-10 10:27:29 +0300

    Update 0.0.1.cpp

--- 0.0.1.cpp
+++ 0.0.1.cpp
@@ -1,1 +1,44 @@
-# test_0.0.2
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+bool read(double *array, unsigned int size){
+    string stroka;
+    getline(cin, stroka);
+    istringstream stream(stroka);
+    for(unsigned int i=0; i<size; ++i){
+        if(!(stream>>array[i])){
+            cout<<"An error has occured while reading input data."<<endl;
+            return false;
+        }
+    }
+    return true;
+}
+
+void reverse(double *array, unsigned int size){
+    for(unsigned int i=0; i<size/2; ++i){
+        swap(array[i], array[size-i-1]);
+    }
+}
+
+int main(){
+    unsigned int size;
+    string s;
+    getline(cin, s);
+    istringstream stream(s);
+    if(!(stream>>size)){
+        cout<<"An error has occured while reading input data."<<endl;
+        return -1;
+    }
+    double *array=new double[size];
+    if(read(array, size)){
+        reverse(array, size);
+        for(unsigned int i=0; i<size; ++i){
+            cout<<array[i]<<" ";
+        }
+    }
+    delete[]array;
+    return 0;
+}
